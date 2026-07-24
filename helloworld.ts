3edef9791945893f96a2a810d7ecb3d80b0a876f
dembenski <95358921+dembenski@users.noreply.github.com>
2026-07-24 07:43:50 -0700

    tick

--- helloworld.ts
+++ helloworld.ts
@@ -1,10 +1,5 @@
-import { runStart } from "./Yuu API/RegisterStart";
-import { Vector3 } from "./Yuu API/Basic Types/Vector3";
+console.log("Start");
 
-console.log('Running Start From YuuBlocks');
-
-// If your API supports spawning primitives directly, you can call it here:
-const cubePosition = new Vector3(0, 1.0, -2.0);
-spawnPrimitive.cube(cubePosition);
-
-runStart();
+setInterval(() => {
+    console.log("Tick...");
+}, 1000);
