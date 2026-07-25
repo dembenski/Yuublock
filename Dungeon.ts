5b1e773cc44ba37ba3dbe64ad32764b871adb3bd
dembenski <95358921+dembenski@users.noreply.github.com>
2026-07-24 22:29:53 -0700

    trapped in room

--- Dungeon.ts
+++ Dungeon.ts
@@ -1,255 +1,6 @@
-import { Vector3 } from "./Yuu API/Basic Types/Vector3";
-import { Color } from "./Yuu API/Basic Types/Color";
-import { Quaternion } from "./Yuu API/Basic Types/Quaternion";
-import { Entity } from "./Yuu API/Entity";
-import { Player } from "./Yuu API/Player";
-import { spawnPrimitive } from "./Yuu API/SpawnPrimitive";
-
-import { addEnemyAI } from "./EnemyAI";
-import { registerEnemyCombat } from "./Combat";
-
-
-
-
 // =====================================
-// MAZE SIZE
-// =====================================
-
-const mazeWidth = 31;
-
-const mazeHeight = 31;
-
-const blockSize = 8;
-
-
-let maze:number[][] = [];
-
-
-
-
-export let enemies:any[] = [];
-
-export let chests:Entity[] = [];
-
-
-
-
-
-
-// =====================================
-// CREATE BLOCK
-// =====================================
-
-function cube(
-
-pos:Vector3,
-
-scale:Vector3,
-
-color:Color
-
-):Entity
-
-{
-
-
-return spawnPrimitive.cube(
-
-pos,
-
-scale,
-
-
-Quaternion.fromEuler(
-
-new Vector3(
-
-0,
-
-0,
-
-0
-
-)
-
-),
-
-
-color,
-
-1,
-
-true,
-
-"Static",
-
-undefined
-
-);
-
-
-}
-
-
-
-
-
-
-// =====================================
-// RETRO WALL TEXTURE COLORS
-// =====================================
-
-
-function wallColor(x:number,z:number):Color
-{
-
-
-let pattern = (x+z)%5;
-
-
-
-if(pattern==0)
-
-{
-
-return new Color(
-
-.55,
-
-.45,
-
-.35
-
-);
-
-}
-
-
-
-if(pattern==1)
-
-{
-
-return new Color(
-
-.42,
-
-.35,
-
-.28
-
-);
-
-}
-
-
-
-if(pattern==2)
-
-{
-
-return new Color(
-
-.32,
-
-.30,
-
-.28
-
-);
-
-}
-
-
-
-if(pattern==3)
-
-{
-
-return new Color(
-
-.25,
-
-.22,
-
-.18
-
-);
-
-}
-
-
-
-return new Color(
-
-.48,
-
-.38,
-
-.30
-
-);
-
-
-}
-
-
-
-
-
-
-
-// =====================================
-// RETRO FLOOR TILE COLORS
-// =====================================
-
-
-function floorColor(x:number,z:number):Color
-{
-
-
-let tile = (x+z)%2;
-
-
-
-if(tile==0)
-
-{
-
-return new Color(
-
-.18,
-
-.15,
-
-.12
-
-);
-
-}
-
-
-
-return new Color(
-
-.10,
-
-.10,
-
-.08
-
-);
-
-
-}
-
-
-
-
-
-
-// =====================================
-// GENERATE MAZE
+// GENERATE CONNECTED MAZE
+// EVERY ROOM HAS OPENINGS
 // =====================================
 
 function generateMaze()
@@ -261,588 +12,266 @@ maze=[];
 
 
 
+// Fill everything with walls
+
 for(let x=0;x<mazeWidth;x++)
 
 {
 
-maze[x]=[];
+    maze[x]=[];
 
 
-for(let z=0;z<mazeHeight;z++)
+    for(let z=0;z<mazeHeight;z++)
 
-{
+    {
 
-maze[x][z]=1;
+        maze[x][z]=1;
+
+    }
 
 }
 
 
-}
 
 
 
-// carve connected paths
+// =================================
+// CARVE CONNECTED PATHS
+// =================================
 
 function carve(x:number,z:number)
 
 {
 
-maze[x][z]=0;
+
+    maze[x][z]=0;
 
 
 
-let dirs =
+    let dirs =
 
-[
+    [
 
-[2,0],
+        [2,0],
 
-[-2,0],
+        [-2,0],
 
-[0,2],
+        [0,2],
 
-[0,-2]
+        [0,-2]
 
-];
-
-
-
-dirs.sort(()=>Math.random()-0.5);
+    ];
 
 
 
-for(let d of dirs)
 
-{
 
-let nx=x+d[0];
-
-let nz=z+d[1];
+    dirs.sort(()=>Math.random()-0.5);
 
 
 
-if(
-
-nx>0 &&
-
-nz>0 &&
-
-nx<mazeWidth-1 &&
-
-nz<mazeHeight-1 &&
-
-maze[nx][nz]==1
-
-)
-
-{
 
 
-maze[x+d[0]/2][z+d[1]/2]=0;
+
+    for(let d of dirs)
+
+    {
 
 
-carve(nx,nz);
+        let nx=x+d[0];
+
+        let nz=z+d[1];
+
+
+
+
+
+        if(
+
+            nx>0 &&
+
+            nz>0 &&
+
+            nx<mazeWidth-1 &&
+
+            nz<mazeHeight-1 &&
+
+            maze[nx][nz]==1
+
+        )
+
+        {
+
+
+            // open wall between cells
+
+            maze[x+d[0]/2][z+d[1]/2]=0;
+
+
+
+            carve(nx,nz);
+
+
+        }
+
+
+    }
 
 
 }
 
 
-}
 
 
-}
 
 
+// create main maze
 
 carve(1,1);
 
 
 
-// guaranteed start
+
+
+
+
+// =================================
+// ADD EXTRA DOORWAYS
+// STOP SEALED ROOMS
+// =================================
+
+for(let x=1;x<mazeWidth-1;x++)
+
+{
+
+
+    for(let z=1;z<mazeHeight-1;z++)
+
+    {
+
+
+        if(maze[x][z]==0)
+
+        {
+
+
+            let exits = 0;
+
+
+
+            if(maze[x+1][z]==0)
+                exits++;
+
+
+            if(maze[x-1][z]==0)
+                exits++;
+
+
+            if(maze[x][z+1]==0)
+                exits++;
+
+
+            if(maze[x][z-1]==0)
+                exits++;
+
+
+
+
+
+            // create another exit if trapped
+
+            if(exits <= 1)
+
+            {
+
+
+                let possible =
+
+                [
+
+                    [1,0],
+
+                    [-1,0],
+
+                    [0,1],
+
+                    [0,-1]
+
+                ];
+
+
+
+
+
+                let choice = possible[
+
+                    Math.floor(
+
+                        Math.random()*possible.length
+
+                    )
+
+                ];
+
+
+
+
+
+                let nx=x+choice[0];
+
+                let nz=z+choice[1];
+
+
+
+
+
+                if(
+
+                    nx>0 &&
+
+                    nz>0 &&
+
+                    nx<mazeWidth-1 &&
+
+                    nz<mazeHeight-1
+
+                )
+
+                {
+
+
+                    maze[nx][nz]=0;
+
+
+                }
+
+
+            }
+
+
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+// =================================
+// GUARANTEED START + EXIT
+// =================================
 
 maze[1][1]=0;
 
 
-// guaranteed exit
-
 maze[mazeWidth-2][mazeHeight-2]=0;
 
 
-}
-
-// =====================================
-// BUILD DUNGEON
-// =====================================
-
-export async function createDungeon()
-
-{
-
-
-generateMaze();
-
-
-
-console.log(
-
-"Generating RETRO WOLFENSTEIN DUNGEON..."
-
-);
-
-
-
-
-
-let offsetX =
-
--(mazeWidth * blockSize)/2;
-
-
-
-let offsetZ =
-
--(mazeHeight * blockSize)/2;
-
-
-
-
-
-
-for(let x=0;x<mazeWidth;x++)
-
-{
-
-
-for(let z=0;z<mazeHeight;z++)
-
-{
-
-
-let worldX =
-
-(x*blockSize)+offsetX;
-
-
-
-let worldZ =
-
-(z*blockSize)+offsetZ;
-
-
-
-
-
-
-
-
-// ================================
-// WALLS
-// ================================
-
-if(maze[x][z]==1)
-
-{
-
-
-cube(
-
-new Vector3(
-
-worldX,
-
-3,
-
-worldZ
-
-),
-
-
-new Vector3(
-
-blockSize,
-
-6,
-
-blockSize
-
-),
-
-
-wallColor(x,z)
-
-);
-
-
-
-}
-
-
-
-
-
-
-
-
-
-// ================================
-// FLOORS
-// ================================
-
-else
-
-{
-
-
-cube(
-
-new Vector3(
-
-worldX,
-
-0,
-
-worldZ
-
-),
-
-
-new Vector3(
-
-blockSize,
-
-.2,
-
-blockSize
-
-),
-
-
-floorColor(x,z)
-
-);
-
-
-
-
-
-
-
-// keep start room empty
-
-if(
-
-!(x==1 && z==1)
-
-)
-
-{
-
-
-spawnObjects(
-
-worldX,
-
-worldZ
-
-);
-
-
-}
-
-
-
-}
-
-
-
-}
-
-
-
-}
-
-
-
-
-
-
-
-// =================================
-// PLAYER START
-// =================================
-
-
-Player.position.set(
-
-new Vector3(
-
-offsetX + blockSize,
-
-1,
-
-offsetZ + blockSize
-
-)
-
-);
-
-
-
-
-
-console.log(
-
-"MAZE COMPLETE"
-
-);
-
-
-console.log(
-
-"PLAYER INSIDE START ROOM"
-
-);
-
-
-
-}
-
-// =====================================
-// SPAWN OBJECTS
-// =====================================
-
-function spawnObjects(
-
-x:number,
-
-z:number
-
-)
-
-{
-
-
-let chance = Math.random();
-
-
-
-
-
-// ================================
-// ENEMIES
-// ================================
-
-if(chance < .12)
-
-{
-
-
-let enemy = cube(
-
-new Vector3(
-
-x,
-
-1,
-
-z
-
-),
-
-
-new Vector3(
-
-1,
-
-2,
-
-1
-
-),
-
-
-new Color(
-
-1,
-
-0,
-
-0
-
-)
-
-);
-
-
-
-
-
-
-let data =
-
-{
-
-entity:enemy,
-
-
-name:getEnemyName(),
-
-
-hp:100,
-
-
-damage:10,
-
-
-alive:true
-
-
-};
-
-
-
-
-
-
-enemies.push(data);
-
-
-
-addEnemyAI(enemy);
-
-
-
-registerEnemyCombat(data);
-
-
-
-console.log(
-
-"Enemy spawned"
-
-);
-
-
-
-}
-
-
-
-
-
-
-
-
-
-// ================================
-// CHESTS
-// ================================
-
-else if(chance < .18)
-
-{
-
-
-let chest = cube(
-
-new Vector3(
-
-x,
-
-1,
-
-z
-
-),
-
-
-new Vector3(
-
-1,
-
-1,
-
-1
-
-),
-
-
-new Color(
-
-1,
-
-.8,
-
-0
-
-)
-
-);
-
-
-
-
-
-chests.push(chest);
-
-
-
-console.log(
-
-"Chest spawned"
-
-);
-
-
-
-}
-
-
-
-}
-
-
-
-
-
-
-
-
-// =====================================
-// ENEMY NAMES
-// =====================================
-
-function getEnemyName()
-
-{
-
-
-let list =
-
-[
-
-"Goblin",
-
-"Skeleton",
-
-"Orc",
-
-"Shadow Beast"
-
-];
-
-
-
-
-
-return list[
-
-Math.floor(
-
-Math.random()*list.length
-
-)
-
-];
 
 }
